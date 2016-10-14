1c1365a926cdaf97590f03f5426642ccbbc73250
Andriy <andriivos@gmail.com>
2016-10-15 01:36:56 +0300

    Fix IQ calibration (write_results() stage)
    This also fixes 'no ACK for OFDM frames' bug; as a result,
    now connection will be significantly faster

--- sys/dev/rtwn/rtl8192c/pci/r92ce_calib.c
+++ sys/dev/rtwn/rtl8192c/pci/r92ce_calib.c
@@ -319,46 +319,38 @@ r92ce_iq_calib_write_results(struct rtwn_softc *sc, uint16_t tx[2],
 	reg = rtwn_bb_read(sc, R92C_OFDM0_TXIQIMBALANCE(chain));
 	val = ((reg >> 22) & 0x3ff);
 	x = tx[0];
-	if (x & 0x0200)
-		x |= 0xfc00;
+	if (x & 0x00000200)
+		x |= 0xfffffc00;
 	reg = (((x * val) >> 8) & 0x3ff);
-	rtwn_bb_write(sc, R92C_OFDM0_TXIQIMBALANCE(chain), reg);
-
-	if (((x * val) >> 7) & 0x01)
-		rtwn_bb_setbits(sc, R92C_OFDM0_ECCATHRESHOLD, 0, 0x80000000);
-	else
-		rtwn_bb_setbits(sc, R92C_OFDM0_ECCATHRESHOLD, 0x80000000, 0);
+	rtwn_bb_setbits(sc, R92C_OFDM0_TXIQIMBALANCE(chain), 0x3ff, reg);
+	rtwn_bb_setbits(sc, R92C_OFDM0_ECCATHRESHOLD, 0x80000000,
+	    ((x * val) & 0x80) << 24);
 
 	y = tx[1];
 	if (y & 0x00000200)
 		y |= 0xfffffc00;
 	tx_c = (y * val) >> 8;
-	rtwn_bb_setbits(sc, R92C_OFDM0_TXAFE(chain), 0,
-	    (((tx_c & 0x3c0) >> 6) << 24) & 0xf0000000);
-
-	rtwn_bb_setbits(sc, R92C_OFDM0_TXIQIMBALANCE(chain), 0,
-	    ((tx_c & 0x3f) << 16) & 0x003F0000);
-
-	if (((y * val) >> 7) & 0x01)
-		rtwn_bb_setbits(sc, R92C_OFDM0_ECCATHRESHOLD, 0, 0x20000000);
-	else
-		rtwn_bb_setbits(sc, R92C_OFDM0_ECCATHRESHOLD, 0x20000000, 0);
+	rtwn_bb_setbits(sc, R92C_OFDM0_TXAFE(chain), 0xf0000000,
+	    (tx_c & 0x3c0) << 22);
+	rtwn_bb_setbits(sc, R92C_OFDM0_TXIQIMBALANCE(chain), 0x003f0000,
+	    (tx_c & 0x3f) << 16);
+	rtwn_bb_setbits(sc, R92C_OFDM0_ECCATHRESHOLD, 0x20000000,
+	    ((y * val) & 0x80) << 22);
 
 	if (rx[0] == 0xff || rx[1] == 0xff)
 		return;
 
-	reg = rtwn_bb_read(sc, R92C_OFDM0_RXIQIMBALANCE(chain));
-	reg |= (rx[0] & 0x3ff);
-	rtwn_bb_write(sc, R92C_OFDM0_RXIQIMBALANCE(chain), reg);
-	reg |= (((rx[1] & 0x03f) << 8) & 0xFC00);
-	rtwn_bb_write(sc, R92C_OFDM0_RXIQIMBALANCE(chain), reg);
+	rtwn_bb_setbits(sc, R92C_OFDM0_RXIQIMBALANCE(chain), 0x3ff,
+	    rx[0] & 0x3ff);
+	rtwn_bb_setbits(sc, R92C_OFDM0_RXIQIMBALANCE(chain), 0xfc00,
+	    (rx[1] & 0x3f) << 10);
 
 	if (chain == 0) {
-		rtwn_bb_setbits(sc, R92C_OFDM0_RXIQEXTANTA, 0,
-		    ((rx[1] & 0xf) >> 6) & 0x000f);
+		rtwn_bb_setbits(sc, R92C_OFDM0_RXIQEXTANTA, 0xf0000000,
+		    (rx[1] & 0x3c0) << 22);
 	} else {
-		rtwn_bb_setbits(sc, R92C_OFDM0_AGCRSSITABLE, 0,
-		    (((rx[1] & 0xf) >> 6) << 12) & 0xf000);
+		rtwn_bb_setbits(sc, R92C_OFDM0_AGCRSSITABLE, 0xf000,
+		    (rx[1] & 0x3c0) << 6);
 	}
 }
 
